e3ebcc9679e773d968764dcac2713ebd8804ebcf
Chong-Kyle <335545380@gapps.yrdsb.ca>
2021-04-06 12:54:45 -0400

    Test 3

--- ColabCustomerSystem.java
+++ ColabCustomerSystem.java
@@ -81,5 +81,5 @@ class CustomerSystem{
     /*******************************************************************
     *       ADDITIONAL METHODS MAY BE ADDED BELOW IF NECESSARY         *
     *******************************************************************/
-    //test 2
+    //test 3
 }
